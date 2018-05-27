7fa39612d272ad4a673f1b89a652b5a1568c5d6a
Mans Matulewicz <mans.matulewicz@gmail.com>
2018-05-27 13:13:22 +0200

    updates

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ HAVERSION=$(curl https://api.github.com/repos/home-assistant/home-assistant/rele
 #rm Dockerfile-local
 sed -e "s/_HAVERSION_/${HAVERSION}/g" homeassistant/Dockerfile |tee homeassistant/Dockerfile-local
 
-docker build --no-cache -f homeassistant/Dockerfile-local -t mansm/homeassistant:${HAVERSION} homeassistant/
+docker build -f homeassistant/Dockerfile-local -t mansm/homeassistant:${HAVERSION} homeassistant/
 docker tag mansm/homeassistant:${HAVERSION} mansm/homeassistant:latest
 
 docker build -t mansm/mqtt:latest mqtt/
